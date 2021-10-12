0b2e4cd77fbf6e6eb6cccc7f83bc60f51374a947
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-10-12 10:35:57 +0200

    User Repositoru

---
+++ Astrow-2.0/Repository/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Astrow_2._0.Model;
+
+namespace Astrow_2._0.Repository
+{
+    interface IUserRepository
+    {
+        void CreateUser(Users user);
+
+        void DeleteUser(Users user);
+
+        void UpdateUser(Users user);
+
+        List<Users> ReadAllUsers();
+
+        Users FindUser(int id);
+
+    }
+}
---
+++ Astrow-2.0/Repository/UserRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Astrow_2._0.Model;
+using Astrow_2._0.DataLayer;
+
+namespace Astrow_2._0.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        StoredProcedure Stored = new StoredProcedure();
+
+        List<Users> userList = new List<Users>();
+
+        Users user = new Users();
+
+        /// <summary>
+        /// Create User
+        /// </summary>
+        /// <param name="user"></param>
+        public void CreateUser(Users user)
+        {
+            Stored.CreateUser(user);
+        }
+
+        /// <summary>
+        /// Delete user from database
+        /// </summary>
+        /// <param name="user"></param>
+        public void DeleteUser(Users user)
+        {
+            Stored.DeleteUser(user);
+        }
+
+        /// <summary>
+        /// Update user parameters
+        /// </summary>
+        /// <param name="user"></param>
+        public  void UpdateUser(Users user)
+        {
+            Stored.UpdateUser(user);
+        }
+
+        /// <summary>
+        /// Return all users to list
+        /// </summary>
+        /// <returns></returns>
+        public List<Users> ReadAllUsers()
+        {
+            if (userList.Count != 0)
+            {
+                userList.Clear();
+            }
+
+            userList = Stored.ReadAllUsers(userList, user);
+
+            return userList;
+        }
+
+        /// <summary>
+        /// Find User by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Users FindUser(int id)
+        {
+            user = Stored.FindByID(id, user);
+            return user;
+        }
+    }
+}
