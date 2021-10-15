9689e052826e009271e523c334d76507bfb48936
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2021-10-15 08:18:20 +0200

    Login back-end begin

--- Astrow-2.0/Pages/Login.cshtml.cs
+++ Astrow-2.0/Pages/Login.cshtml.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Astrow_2._0.Repository;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 namespace Astrow_2._0.Pages
 {
@@ -17,9 +19,39 @@ namespace Astrow_2._0.Pages
             _userRepository = userRepository;
         }
 
+        [BindProperty, Required]
+        public string UserName { get; set; }
+
+        [BindProperty, Required]
+        public string Password { get; set; }
+
 
         public void OnGet()
         {
+
+        }
+
+        public void OnPost()
+        {
+            
+        }
+
+        static byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA256Managed();
+
+            byte[] plainTextWithSaltBytes = new byte[plainText.Length + salt.Length];
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = plainText[i];
+            }
+            for (int i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltBytes);
         }
     }
 }
