415ce95e2f57d2b47851e786ba429c7bd7a8a654
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2022-01-10 09:06:45 +0100

    List of all users

--- Astrow-2.0/Pages/AdminPage/UpdateUser.cshtml.cs
+++ Astrow-2.0/Pages/AdminPage/UpdateUser.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Astrow_2._0.Model.Containers;
 using Astrow_2._0.Repository;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace Astrow_2._0.Pages.AdminPage
 {
@@ -17,24 +18,36 @@ namespace Astrow_2._0.Pages.AdminPage
             _userRepository = userRepository;
         }
 
+        [BindProperty]
+        public List<Users> UserList { get; set; }
+
+        [BindProperty, Required(ErrorMessage = "* Brugernavn skal udfyldes")]
+        public string UserName { get; set; }
+
 
         [BindProperty, Required(ErrorMessage = "* Fornavn skal udfyldes")]
         public string FirstName { get; set; }
 
+
         [BindProperty, Required(ErrorMessage = "* Efternavn skal udfyldes")]
         public string LastName { get; set; }
+
 
         [BindProperty, Required(ErrorMessage = "* Kodeord skal udfyldes")]
         public string Password { get; set; }
 
+
         [BindProperty, Required(ErrorMessage = "* Rolle skal udfyldes")]
         public string Role { get; set; }
+
 
         [BindProperty, Required(ErrorMessage = "* Start dato skal udfyldes")]
         public string StartDate { get; set; }
 
+
         [BindProperty, Required(ErrorMessage = "* Slut dato skal udfyldes")]
         public string EndDate { get; set; }
+
 
         public IActionResult OnGet()
         {
@@ -44,8 +57,27 @@ namespace Astrow_2._0.Pages.AdminPage
             }
             else
             {
+                UserList = _userRepository.ReadAllUsers();
+
                 return Page();
             }
+        }
+
+        public Users GetUserInfo()
+        {
+            string name = HttpContext.Session.GetString("_Username");
+
+            string status = HttpContext.Session.GetString("_Status");
+
+            Users user = new Users();
+
+
+            return user = new Users
+            {
+
+            };
+
+
         }
     }
 }
